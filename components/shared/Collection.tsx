42d50c2b04482bdfa4a64eef2051aa3045e02413
Me-V <gonemanarmy@gmail.com>
2024-08-23 06:00:04 +0530

    Fixed

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -56,7 +56,8 @@ export const Collection = ({
       {images.length > 0 ? (
         <ul className="collection-list">
           {images.map((image) => (
-            <Card image={image} key={image._id} />
+            <Card image={image} key={image._id as string | number} /> // THIS LINE WAS CHANGED ##################
+
           ))}
         </ul>
       ) : (
