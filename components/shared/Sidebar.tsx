b6a976d0deef25b6bf9dbf9177dc81253bb64f53
Me-V <gonemanarmy@gmail.com>
2024-08-23 05:49:21 +0530

    Fixed Stuff

---
+++ components/shared/Sidebar.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import { navLinks } from '@/constants'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button'
+
+const Sidebar = () => {
+  const pathname = usePathname();
+
+  return (
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
+        </Link>
+
+        <nav className="sidebar-nav">
+          <SignedIn> 
+{/* The code written within the signed in coponent of the clerk is only shown is the user is logged in###############################################################*/}
+            <ul className="sidebar-nav_elements">
+              {/* slice is used to push the other elements of the sidebar to the bottom */}
+              {navLinks.slice(0, 6).map((link) => {
+                const isActive = link.route === pathname
+// isActive helps finding out if we are already on a specific link from the list of links
+                return (
+
+                  // key is link.route coz link.route is always unique
+                  <li key={link.route} className={`sidebar-nav_element group ${
+                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                  }`}>
+                    {/* Note:- link.route is coming from the navLinks from the constant file  */}
+                    <Link className="sidebar-link" href={link.route}> 
+                      <Image 
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                )
+              })}
+              </ul>
+
+
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(6).map((link) => {
+                const isActive = link.route === pathname
+
+                return (
+                  <li key={link.route} className={`sidebar-nav_element group ${
+                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                  }`}>
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image 
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                )
+              })}
+
+              <li className="flex-center cursor-pointer gap-2 p-4">
+                <UserButton afterSignOutUrl='/' showName />
+              </li>
+            </ul>
+          </SignedIn>
+           
+           {/* when we are not signed in --- this code will be executed */}
+          <SignedOut>
+            {/* asChild property is to make this button a LINK  */}
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </nav>
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
---
+++ lib/database/mongoose.ts
@@ -0,0 +1,32 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose // For making the cache of the webapp
+
+if(!cached) { // If cahche doesnt exist then making the newer one ... 
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
+}
+
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn; //This is the optimization that the next js provides since we directly comes to conclusion the db is already connected if the cache is found in the browser #######################################
+
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
+    })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn; //From the line 18 to 31 is how the MongoDB connections are made ################################
+}
